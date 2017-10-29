d74b218660e63cb589750e240e525b44bbd24136
Minhui Xie <xiemhemail@gmail.com>
2017-10-29 21:39:11 +0800

    add history (arraylist implement)

--- Monet/History.cs
+++ Monet/History.cs
@@ -13,14 +13,33 @@ namespace Monet
         internal Tool Tool { get => tool; set => tool = value; }
         internal ToolParameters ToolParameters { get => toolParameters; set => toolParameters = value; }
     }
+
+    ///-------------------------------------------------------------------------------------------------
+    /// \class History
+    ///
+    /// \brief A singleton class stores all what user draw step by step.
+    ///        And it can be used to undo and redo operations 
+    ///-------------------------------------------------------------------------------------------------
+
     class History
     {
         private ArrayList actionsArray;
-
+        /// \brief Zero-based index of the last valid element in array.
+        int index;
+        /// \brief The instance.
         static History mInstance;
+
+        private bool canUndo;
+        public bool CanUndo { get => canUndo; }
+
+        private bool canRedo;     
+        public bool CanRedo { get => canRedo;}
+
         private History()
         {
-            ;
+            index = 0 ;
+            canUndo = canRedo = false;
+            actionsArray = new ArrayList();
         }
         public static History GetInstance()
         {
@@ -33,7 +52,39 @@ namespace Monet
 
         public void PushBackAction(Action action)
         {
-            actionsArray.Add(action);
+            if (index == actionsArray.Capacity - 1)
+            {
+                actionsArray.Add(action);
+                index++;
+            }
+            else
+            {
+                actionsArray[++index] = action;
+            }
+            canUndo = true;
+        }
+
+        public Action UndoAction()
+        {
+            if (index <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (index == 1)
+                canUndo = false;
+
+            canRedo = true;
+            return actionsArray[--index] as Action;
+        }
+
+        public Action RedoAction()
+        {
+            if (index == actionsArray.Count - 1)
+                throw new ArgumentOutOfRangeException();
+            else if (index == actionsArray.Count - 2)
+                canRedo = false;
+
+            return actionsArray[++index] as Action;
         }
     }
 }
--- Monet/ToolKit.cs
+++ Monet/ToolKit.cs
@@ -9,14 +9,45 @@ namespace Monet
 {
     class ToolKit
     {
+        /// \brief The main view control
         PictureBox mainView;
+        /// \brief The current tool
         public Tool currentTool;
 
+        /// \brief The pointer tool
         static Tool pointerTool;
+        /// \brief The line tool
         static Tool lineTool;
-        
+
+        ///-------------------------------------------------------------------------------------------------
+        /// \property internal Tool LineTool
+        ///
+        /// \brief Gets the line tool
+        ///
+        /// \return The line tool.
+        ///-------------------------------------------------------------------------------------------------
+
         internal Tool LineTool { get => lineTool; }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// \property internal Tool PointerTool
+        ///
+        /// \brief Gets the pointer tool
+        ///
+        /// \return The pointer tool.
+        ///-------------------------------------------------------------------------------------------------
+
         internal Tool PointerTool { get => pointerTool;}
+
+        ///-------------------------------------------------------------------------------------------------
+        /// \fn internal ToolKit(PictureBox mainView, Button pointerButton, Button lineButton)
+        ///
+        /// \brief Constructor
+        ///
+        /// \param mainView      The main view control.
+        /// \param pointerButton The pointer control.
+        /// \param lineButton    The line control.
+        ///-------------------------------------------------------------------------------------------------
 
         internal ToolKit(PictureBox mainView,
                          Button pointerButton,
